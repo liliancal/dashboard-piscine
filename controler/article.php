99d270af997e2e12e458c7dd8c2e65e633eb72b0
Lili <lcalliot.contact@gmail.com>
2019-03-08 11:34:12 +0100

    nouvelle version avec twig

--- controler/article.php
+++ controler/article.php
@@ -2,9 +2,17 @@
 $pageTitle="Listearticles";
 // HTML 
 include('model/article.php');         
-include('view/head_table.php');          
-include('view/topbar.php');    
-include('view/sidebar.php');             
-include('view/article.php');
-include('view/footer.php');        
-include('view/footer_table.php');  
+// include('view/head_table.php');          
+// include('view/topbar.php');    
+// include('view/sidebar.php');             
+// include('view/article.php');
+// include('view/footer.php');        
+// include('view/footer_table.php');  
+echo $twig->render('table.html', 
+array('title' => 'Liste des utilisateurs', 
+'assets' => 'assets/',
+'name' => $_SESSION['surname']." ".$_SESSION['name'],
+'projectName' => "LaPiscine",
+'projectNameShort' => "L-P",
+'list' => $row )
+);
